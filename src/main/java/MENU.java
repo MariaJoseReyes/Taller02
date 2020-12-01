05e6b6186eb2cb8f68a9c617ab0309bb325aa48d
MariaJoseReyes <64438037+MariaJoseReyes@users.noreply.github.com>
2020-12-01 10:33:43 -0300

    Add files via upload

---
+++ src/main/java/MENU.java
@@ -0,0 +1,17 @@
+public class MENU {
+
+    public static void main(String[] args) {
+
+        System.out.println("Bienvenidx a la pagina de registros sismicos");
+        System.out.println("¿Que desea hacer?");
+        System.out.println("[1] ¿Cuando ocurrio el sismo mas intenso?");
+        System.out.println("[2] ¿Cuantos dias ocurrieron sismos sobre o igual 5.5 grados?");
+        System.out.println("[3] ¿Hay alerta de escalada sismica?");
+
+        
+        switch (opcion){
+
+
+        }
+    }
+}
