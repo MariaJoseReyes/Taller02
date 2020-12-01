9bd7526e3ea8bcf879efe778039c4b1d81a431bb
MariaJoseReyes <64438037+MariaJoseReyes@users.noreply.github.com>
2020-12-01 09:39:13 -0300

    Add files via upload

---
+++ src/main/java/Sismologia.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Sismologia {
+
+    public static int cantidadDias() {
+        Scanner leer = new Scanner(System.in);
+        int cant = 31;
+        try {
+            while (cant < 1 || cant > 30) {
+                System.out.println("Ingrese la cantidad de dias");
+                cant = leer.nextInt();
+                if (cant < 1 || cant > 30) {
+                    System.out.println("Error, el numero de dias debe ser entre 1 y 30");
+                }
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Error, debe ser un numero entero");
+        } catch (RuntimeException e) {
+            System.out.println("Error con el tiempo de uso");
+
+        }
+        return cant;
+    }
+
+    
+}
