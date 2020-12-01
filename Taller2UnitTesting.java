1147d2d6717a8fc94ed771aaf41ec8ab9628e594
fabopereira20 <73892102+fabopereira20@users.noreply.github.com>
2020-12-01 10:18:05 -0300

    Add files via upload
    Avance

--- Taller2UnitTesting.java
+++ Taller2UnitTesting.java
@@ -1,23 +1,49 @@
+import java.io.IOException;
 import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Taller2UnitTesting {
-    public int verificarSismos() {
-        int dias;
-        do {
-            try {
-                Scanner leer = new Scanner(in);
-                dias = leer.nextInt();
-            } catch (Exception e) {
-                System.out.println("Dato inválido");
+
+    public static int cantidadDias() {
+        Scanner leer = new Scanner(System.in);
+        int cant = 31;
+        try {
+            while (cant < 1 || cant > 30) {
+                System.out.println("Ingrese la cantidad de dias");
+                cant = leer.nextInt();
+                if (cant < 1 || cant > 30) {
+                    System.out.println("Error, el numero de dias debe ser entre 1 y 30");
+                }
             }
-        }while(true);
-    int[][] datosSensor = new int[dias][24];
-        for (int x=0; x < datosensor.length; x++){
-            for (int y=0; y < datosSensor[x].length; y++) {
+        } catch (InputMismatchException e) {
+            System.out.println("Error, debe ser un numero entero");
+        } catch (RuntimeException e) {
+            System.out.println("Error con el tiempo de uso");
+
+        }
+        return cant;
+    }
+
+    public static double[][] crearArreglo(int n) {
+        double[][] sismos = new double[n][24];
+        return sismos;
+    }
+
+    public static double[][] sismosRandom (double[][] sismos) {
+        Random azar = new Random();
+
+        for(int i=0; i<sismos.length; i++){
+            for(int j=0; j<24; j++) {
+                sismos[i][j] = Math.random();
             }
         }
+        return sismos;
+    }
+}
 
-    /*public int[] ordenarMayorMenor(int[] valores) {
+
+
+/*public int[] ordenarMayorMenor(int[] valores) {
         for (int x = 0; x < valores.length; x++) {
             for (int i = 0; i < valores.length - x - 1; i++) {
                 if (valores[i] < valores[i + 1]) {
@@ -29,4 +55,4 @@ public class Taller2UnitTesting {
         }
         return valores;*/
     }
-}
+}*/
